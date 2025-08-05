21861dea442bbb7fda4b451fa6c0cc140260fc52
Yasmin Garcia Borges <84362966+yasmingarciab@users.noreply.github.com>
2025-08-05 20:56:34 -0300

    primeiro commit

---
+++ calculadora-imc.js
@@ -0,0 +1,16 @@
+    let p = 60
+    let alt = 1.60
+    
+    const imc = p/alt;
+
+    if (imc < 18.5){
+        console.log("Abaixo do peso")
+    } else if (imc > 18.5 && imc < 24.9){
+        console.log("Peso ideal")
+    } else if (imc > 24.9 && imc < 30){
+        console.log("Obesidade grau 1")
+    } else if (imc > 30 && imc < 35){
+        console.log("Obesidade grau 2")
+    } else {
+        console.log("Obesidade grau 3")
+    }
